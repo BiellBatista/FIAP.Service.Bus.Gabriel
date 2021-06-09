7c11dc1eb0928aeff255dd67bb6d199d8b5d63e0
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-06-08 22:57:18 -0300

    Add project files.

---
+++ FIAP.Service.Bus.Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FIAP.Service.Bus.Consumer
+{
+    public class Program
+    {
+        private const string QueueConnectionString = "Endpoint=sb://gabrielbatistafiap.servicebus.windows.net/;SharedAccessKeyName=ProductPolicy;SharedAccessKey=UkjV63E0MCwFTMtn5h6zlhVULaJdnRdh9IXEyE0xH9s=";
+        private const string QueuePath = "productchanged";
+        private static IQueueClient _queueClient;
+
+        private static void Main(string[] args)
+        {
+            ReceiveMessagesAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task ReceiveMessagesAsync()
+        {
+            _queueClient = new QueueClient(QueueConnectionString, QueuePath);
+            _queueClient.RegisterMessageHandler(MessageHandler,
+                new MessageHandlerOptions(ExceptionHandler) { AutoComplete = false });
+            Console.ReadLine();
+            await _queueClient.CloseAsync();
+        }
+
+        private static Task ExceptionHandler(ExceptionReceivedEventArgs exceptionArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionArgs.Exception}.");
+            var context = exceptionArgs.ExceptionReceivedContext;
+            Console.WriteLine($"Endpoint:{context.Endpoint}, Path:{context.EntityPath}, Action:{context.Action}");
+            return Task.CompletedTask;
+        }
+
+        private static async Task MessageHandler(Message message, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Received message: { Encoding.UTF8.GetString(message.Body)}");
+
+            await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+    }
+}
---
+++ FIAP.Service.Bus.Producer/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP.Service.Bus.Producer
+{
+    public class Program
+    {
+        private const string QueueConnectionString = "Endpoint=sb://gabrielbatistafiap.servicebus.windows.net/;SharedAccessKeyName=ProductPolicy;SharedAccessKey=UkjV63E0MCwFTMtn5h6zlhVULaJdnRdh9IXEyE0xH9s=";
+        private const string QueuePath = "productchanged";
+        private static IQueueClient _queueClient;
+
+        private static void Main(string[] args)
+        {
+            SendMessagesAsync().GetAwaiter().GetResult();
+            Console.WriteLine("messages were sent");
+            Console.ReadLine();
+        }
+
+        private static async Task SendMessagesAsync()
+        {
+            _queueClient = new QueueClient(QueueConnectionString, QueuePath);
+
+            var messages = "Hi,Hello,Hey,How are you,Be Welcome"
+                .Split(",")
+                .Select(msg =>
+                {
+                    Console.WriteLine($"Will send message: {msg}");
+                    return new Message(Encoding.UTF8.GetBytes(msg));
+                })
+                .ToList();
+
+            var sendTask = _queueClient.SendAsync(messages);
+
+            await sendTask;
+            CheckCommunicationExceptions(sendTask);
+
+            var closeTask = _queueClient.CloseAsync();
+
+            await closeTask;
+            CheckCommunicationExceptions(closeTask);
+        }
+
+        private static bool CheckCommunicationExceptions(Task task)
+        {
+            if (task.Exception == null || task.Exception.InnerExceptions.Count == 0) return true;
+
+            task.Exception.InnerExceptions.ToList()
+                .ForEach(innerException =>
+                {
+                    Console.WriteLine($"Error in SendAsync task: { innerException.Message}. Details: { innerException.StackTrace}");
+
+                    if (innerException is ServiceBusCommunicationException)
+                        Console.WriteLine("Connection Problem with Host");
+                });
+
+            return false;
+        }
+    }
+}
